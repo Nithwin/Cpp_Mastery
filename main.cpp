778deee9fa2a9100c967c5be1705357dc46125ee
Nithwin <vmnithwin@gmail.com>
2026-04-15 22:59:27 +0530

    Lamda Expression

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 template <typename T1, typename T2>
 class NetworkPair {
-    private:
+    public:
         T1 first;
         T2 second;
     public:
@@ -18,9 +20,16 @@ class NetworkPair {
 };
 
 int main(){
-    NetworkPair np(3,5);
-    np.print();
-    NetworkPair np2("Hello","World");
-    np2.print();
+    vector<NetworkPair<string,int>> servers;
+    servers.push_back({"10.0.0.1", 8080});
+    servers.push_back({"192.168.1.5", 22});
+    servers.push_back({"172.16.0.1", 443});
+
+    sort(servers.begin(), servers.end(), [](const NetworkPair<string,int> &a,const NetworkPair<string,int> &b){
+        return a.second < b.second;
+    });
+    for(auto &server: servers){
+        server.print();
+    }
     return 0;
 }
