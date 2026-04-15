fc0ecf478f52b88ce3abd791a637a01de671a279
Nithwin <vmnithwin@gmail.com>
2026-04-15 12:09:30 +0530

    Refactor main.cpp to remove swap functions and implement dynamic memory allocation

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void swapWithPointers(int* ptr1, int* ptr2){
-    int tmp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = tmp;
-}
-
-void swapWithReferences(int& ref1, int& ref2){
-    int tmp = ref1;
-    ref1 = ref2;
-    ref2 = tmp;
-}
 int main(){
-    int a = 10; int b = 20;
-    cout << a << " " << b << " \n";
-    swapWithPointers(&a,&b);
-    cout << a << " " << b << " \n";
-    swapWithReferences(a, b);
-    cout << a << " " << b << " \n";
+    int* heapInt = new int; 
+    *heapInt = 42;
+    delete heapInt; 
+    heapInt = nullptr;
     return 0;
 }
