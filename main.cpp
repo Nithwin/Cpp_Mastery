91bd9ea27639c9607404241be5abe84f0ec65120
Nithwin <vmnithwin@gmail.com>
2026-04-15 12:25:51 +0530

    Refactor main.cpp to implement polymorphism with NetworkDevice, Firewall, and Switch classes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Size of Array: ";
-    int size; cin >> size;
-    int *arr = new int[size];
-    for(int i = 0; i < size; i++){
-        arr[i]= i+1;
+class NetworkDevice{
+    public:
+    virtual void processPacket(){
+        cout << "Process Packet default...";
     }
-    for(int i = 0; i < size; i++){
-        cout << arr[i] << " ";
+
+    virtual ~NetworkDevice(){
+        cout << "Network Device Destroyed...";
+    }
+};
+
+class Firewall: public NetworkDevice {
+    public:
+    void processPacket() override {
+        cout << "Firewall Packet...";
+    }
+
+    ~Firewall(){
+        cout << "Firewall Destroyed...";
+    }
+
+};
+
+class Switch: public NetworkDevice {
+    public:
+    void processPacket() override {
+        cout << "Switch Packet Processor...";
+    }
+
+    ~Switch(){
+        cout << "Switch Destoryed...";
+    }
+};
+
+int main(){
+    NetworkDevice** arr = new NetworkDevice*[2];
+    arr[0] = new Firewall;
+    arr[1]  = new Switch;
+    for(int i = 0; i < 2; i++){
+        arr[i]->processPacket();
+    }
+    for(int i = 0; i < 2; i++){
+        delete arr[i];
     }
     delete[] arr;
     return 0;
