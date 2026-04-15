2f8bf9f9b3b72224ec7b9c3de0a8b9f7d94a3ca3
Nithwin <vmnithwin@gmail.com>
2026-04-15 22:24:49 +0530

    Refactor main.cpp to remove TCP server implementation and retain maximum value function

--- main.cpp
+++ main.cpp
@@ -1,91 +1,15 @@
 #include <iostream>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <cstring>
-#include <thread>
-
+#include <string>
 using namespace std;
 
-void handleClient(int client_socket) {
-    char buffer[1024] = {0}; // A bucket to hold the incoming text
-    
-    // Send welcome message
-    const char* hello = "Welcome! Type a message:\n";
-    send(client_socket, hello, strlen(hello), 0);
-    
-    // Infinite loop to keep talking to this specific client
-    while(true) {
-        memset(buffer, 0, 1024); // Clear the bucket
-        
-        // recv() waits until the client types something
-        int bytes_received = recv(client_socket, buffer, 1024, 0);
-        
-        // If they disconnect, bytes_received is 0 or less
-        if (bytes_received <= 0) {
-            cout << "Client disconnected.\n";
-            break; 
-        }
-        
-        cout << "Client says: " << buffer;
-        
-        // Echo it back!
-        send(client_socket, buffer, bytes_received, 0);
-    }
-    close(client_socket); // Hang up the phone when they leave
+template <typename T>
+T getMaximum(T a, T b){
+    return (a > b) ? a: b;
 }
 
-int main() {
-    int server_fd, client_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    // ---------------------------------------------------------
-    // TODO 1: Create the raw socket. 
-    // We want IPv4 (AF_INET) and TCP (SOCK_STREAM).
-    // server_fd = ?????(AF_INET, SOCK_STREAM, 0);
-    // ---------------------------------------------------------
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) { cout << "Socket creation failed\n"; return -1; }
-
-    // Configuration for the port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Listen on any IP address
-    address.sin_port = htons(8080);       // Listen on Port 8080
-    cout << "Ip add: " << address.sin_addr.s_addr <<"\n";
-
-    // ---------------------------------------------------------
-    // TODO 2: Attach the socket to Port 8080.
-    // ?????(server_fd, (struct sockaddr *)&address, sizeof(address));
-    // ---------------------------------------------------------
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    // ---------------------------------------------------------
-    // TODO 3: Tell the socket to wait for incoming connections.
-    // ?????(server_fd, 3); // 3 is the maximum queue size
-    // ---------------------------------------------------------
-    listen(server_fd, 3);
-    cout << "Server is running. Waiting for connections on Port 8080...\n";
-
-    // ---------------------------------------------------------
-    // TODO 4: Wait and grab the first person who connects!
-    // client_socket = ?????(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    // ---------------------------------------------------------
-    cout << "A Client has connected!\n";
-    while(true){
-        client_socket = accept(server_fd,(struct sockaddr *)&address, (socklen_t*)&addrlen);
-        thread worker(handleClient, client_socket);
-        worker.detach();
-    }
-
-    // We send a welcome message using the TCP socket!
-    const char* hello = "Welcome to the Alcatel-Lucent Trial Server!\n";
-    send(client_socket, hello, strlen(hello), 0);
-    cout << "Message sent to client.\n";
-
-    // Close the sockets to prevent memory and file descriptor leaks
-    close(client_socket);
-    close(server_fd);
-
+int main(){
+    cout << getMaximum(4,55) << " \n";
+    cout << getMaximum(4.44,55.55) << " \n";
+    cout << getMaximum('z','a') << " \n";
     return 0;
 }
